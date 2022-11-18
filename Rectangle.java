e38e5fe1960ed92ffb412b1f56947fa61eba1f66
Shubham-1618 <118455449+Shubham-1618@users.noreply.github.com>
2022-11-18 19:35:42 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,15 @@
+public class Rectangle 
+{
+
+	public static void main(String[] args)
+	{
+		
+			float b=7,l=5,area;
+			area=l*b;
+			System.out.println("Area of rectangle is:" +area);
+
+		
+
+	}
+
+}
